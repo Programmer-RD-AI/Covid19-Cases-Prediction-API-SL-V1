fa798fdba7d895bba026f2ea9e86ed059d661881
Programmer-RD-AI <aigithub@gmail.com>
2021-05-10 13:10:48 +0530

    init commit

--- app.py
+++ app.py
@@ -1,31 +1,8 @@
-import numpy as np
-import pickle
-from sklearn.tree import DecisionTreeRegressor
-import os
-from flask import *
-from flask_restful import *
-from flask_restful import reqparse
-
-app = Flask(__name__)
-app.debug = True
-app.secret_key = "This Key is secret_key for sure"
-api = Api(app)
-files = os.listdir("./models")
-
-
-class Pred(Resource):
-    def get(self, date):
-        print(date)
-        results = {}
-        for file in files:
-            model = pickle.load(open(f"./models/{file}", "rb"))
-            preds = model.predict(np.array(date).astype(np.float32).reshape(1,-1))
-            print(file)
-            print(preds)
-            file = str(file.replace(".pkl", ""))
-            results[file] = list(preds)[0]
-        print('returning...')
-        return results
-
-api.add_resource(Pred, "/<int:date>")
-
+from api import app
+from updating import update
+import threading
+if __name__ == "__main__":
+    print('g')
+    threading.Thread(target=app.run(threaded=True, port=5000)).start()
+    print('g')
+    threading.Thread(target=update()).start()
--- run.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from updating import update
-import threading
-if __name__ == "__main__":
-    print('g')
-    threading.Thread(target=app.run(threaded=True, port=5000)).start()
-    print('g')
-    threading.Thread(target=update()).start()
